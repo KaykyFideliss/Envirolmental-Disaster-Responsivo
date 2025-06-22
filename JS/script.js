15addaa2531725d16c22e15aed3fe3951e778098
Kayky Fidelis <kayky.fid@gmail.com>
2025-06-22 14:26:16 -0300

    ESTOU Mexendo na aba videoaula

--- JS/script.js
+++ JS/script.js
@@ -103,24 +103,3 @@ function initializeSlider(sliderId) {
   
   initializeSlider("slidermais-1");
   
-  /* js da NAVBAR ---- TROCA DE COR  */
-  window.addEventListener('scroll', function () {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) {
-      navbar.classList.remove('navbar-transparent' );
-    } else {
-      navbar.classList.add('navbar-transparent');
-    }
-  });
-
-  
-
-  window.addEventListener('scroll', function () {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) {
-      navbar.classList.remove('dropdown-menu' );
-    } else {
-      navbar.classList.add('dropdown-menu');
-    }
-  });
-
